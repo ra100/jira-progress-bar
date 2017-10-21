bb7da1795dbee1ddbe021735bdac5880b3bef866
Svarba, Rastislav <rastislav.svarba@merck.com>
2017-10-21 16:30:05 +0200

    ➕ Add cors server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const request = require('request')
+const { jiraUrl, user, password, port } = require('./config/config.local')
+
+const app = express()
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
+app.use('/api/search', (req, res) => {
+  const options = {
+    url: `${jiraUrl}/rest/api/2/search`,
+    headers: {
+      Authorization: `Basic ${Buffer.from(`${user}:${password}`).toString('base64')}`
+    },
+    qs: req.query,
+    method: 'GET'
+  }
+  req.pipe(request(options)).pipe(res)
+})
+
+app.use(express.static('build'))
+
+app.listen(port)
+
+console.log(`Server listening on port ${port}`) // eslint-disable-line no-console
